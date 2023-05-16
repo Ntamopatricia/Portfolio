679c67887b416cb4b2ee5f4466ede5a840995715
Ntamopatricia <ntamopatricia274@gmail.com>
2023-05-16 18:32:19 +0100

    first commit

---
+++ portfolio/src/components/header/CTA.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+//import CV from '.././../assets/CV FOFIE.pdf'
+const CTA = () => {
+return (
+<div className='cta'>
+
+<a href="#contact" className='btn btn-primary'>Let's Talk</a>
+</div>
+)
+}
+export default CTA
