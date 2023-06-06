b8136e05fd937b3708b29ace1892a7a6e3700245
Ntamopatricia <133794663+Ntamopatricia@users.noreply.github.com>
2023-06-06 11:10:04 +0100

    i improved my frondend

--- portfolio/src/components/services/Services.jsx
+++ portfolio/src/components/services/Services.jsx
@@ -1,9 +1,67 @@
 import React from 'react'
+import './services.css'
+import {BiCheck} from 'react-icons/bi'
 
-export default function Services() {
+const services = () => {
   return (
-    <div>
-      <h1>Services</h1>
-    </div>
+    <section id="services">
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+      <div className='container services__container'>
+        <div>
+          <article className='services'><div className='service__head'>
+            <h3>Graphic design</h3>
+          </div>
+          <ul className='services__list'>
+            <li>
+              <BiCheck className='services__list-icon'/>
+              <p>Flyer</p>
+            </li>
+          </ul><ul className='services__list'>
+            <li>
+              <BiCheck className='services__list-icon'/>
+              <p>Logo</p>
+            </li>
+          </ul><ul className='services__list'>
+            <li>
+              <BiCheck className='services__list-icon'/>
+              <p>Content writing</p>
+            </li>
+          </ul>
+          </article>
+        </div>
+
+
+        <div>
+          <article className='services'><div className='service__head'>
+            <h3>Beauty</h3>
+          </div>
+          <ul className='services__list'>
+            <li>
+              <BiCheck className='services__list-icon'/>
+              <p>Hair dressing</p>
+            </li>
+          </ul><ul className='services__list'>
+            <li>
+              <BiCheck className='services__list-icon'/>
+              <p>skin care</p>
+            </li>
+          </ul><ul className='services_list'>
+          <li>
+              <BiCheck className='services__list-icon'/>
+              <p>Manicure and pedicure</p>
+            </li>
+          </ul>
+          
+
+          </article>
+        </div>
+
+
+      </div>
+    </section>
   )
 }
+
+
+export default services
