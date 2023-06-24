fada9b9f0220c5406cad53317bebf85d025a23c1
Ntamopatricia <133794663+Ntamopatricia@users.noreply.github.com>
2023-06-24 06:49:42 +0100

    c

--- portfolio/src/components/portfolio/Portfolio.jsx
+++ portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,29 +1,105 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'; 
+import axios from 'axios';
 import './portfolio.css'
-import IMG1 from '../../assets/pati 2.jpg'
-import IMG2 from '../../assets/pati 2.jpg'
+import IMG1 from '../../assets/port1.png'
+import IMG2 from '../../assets/port2.png'
+import IMG3 from '../../assets/port3.png'
+import IMG4 from '../../assets/port4.png'
+import IMG5 from '../../assets/port5.png'
+import IMG6 from '../../assets/port6.png'
+
+const data = [
+    {
+        id: 1,
+        image: IMG1,
+        title: 'Data Analysis Research Samples',
+        github: 'https://github.com',
+        demo: 'https://dribbble.com/Alien_pixels'
+    },
+    {
+        id: 2,
+        image: IMG2,
+        title: 'Website sample',
+        github: 'https://github.com',
+        demo: 'https://dribbble.com/Alien_pixels'
+    },
+    {
+        id: 3,
+        image: IMG3,
+        title: 'Figma dashboard UI kit for data design and web apps',
+        github: 'https://github.com',
+        demo: 'https://dribbble.com/Alien_pixels'
+    },
+    {
+        id: 4,
+        image: IMG4,
+        title: 'Maintaining tasks and tracking progress',
+        github: 'https://github.com',
+        demo: 'https://dribbble.com/Alien_pixels'
+    },
+    {
+        id: 5,
+        image: IMG5,
+        title: 'School Management System',
+        github: 'https://github.com',
+        demo: 'https://dribbble.com/Alien_pixels'
+    },
+    {
+        id: 6,
+        image: IMG6,
+        title: 'Charts template & infographics in Figma ',
+        github: 'https://github.com',
+        demo: 'https://dribbble.com/Alien_pixels'
+    },
+]
 
 const Portfolio = () => {
-  return (
-    <section id="portfolio">
-      <h5>My recent work</h5>
-      <h2>Portfolio</h2>
 
-      <div className='container portfolio__container'>
-        <article className='portfolio__item vvv'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='portfolio' />
-          </div>
-          <h3>My Portfolio</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com/Ntamopatricia/portfolio' className='btn' target='_blank' rel="noreferrer">Github</a>
-            <a href='https://647975b42d520a4fa9a2655a--poetic-pothos-1a61f8.netlify.app/' className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
-          </div>
-        </article>
-      </div>
-    </section>
-  )
+  const[data , setData] = useState([]);
+  useEffect (()  => {
+    axios.get("https://woche-liz.cyclic.app/api/portfolio").then(response => {
+      const formattedData = response.data.data.map(item => ({
+        id:item._id,
+        image: item.image,
+        title:item.github,
+        demo:item.demo
+    }));
+    setData(formattedData)
+    }).catch(error=> {
+         console.log("Error fteching the portfolio data",error);
+
+    } )
+    
+    
+  },  []);
+    return (
+        <section id='portfolio'>
+            <h5>My Recent Work</h5>
+            <h2>Portfolio</h2>
+
+            <div className="container portfolio__container">
+                {
+
+
+                    data.map(({ id, image, title, github, demo }) => (
+                            <article key={id} className='portfolio__item'>
+                                <div className="portfolio__item-image">
+                                    <img src={image} alt={title} />
+                                </div>
+                                <h3>{title}</h3>
+                                <div className="portfolio__item-cta">
+                                    <a href={github} className='btn' target='_blank'>GitHub</a>
+                                    <a href={demo} target='_blank' className='btn btn-primary'>Live Demo</a>
+                                </div>
+                            </article>
+                        ))
+
+
+
+               }
+            </div>
+        </section>
+    )
 }
-
 
 export default Portfolio
---
+++ src/components/about/About.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './about.css'
+import moi from '../../assets/pati.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
+const About = () => {
+    return (
+      <section id="about">
+        <h5>Get to know</h5>
+        <h2>About me</h2>
+        <div className="container about__container">
+          <div className="about__me">
+            <div className="about__me__image">
+              <img src={moi} alt="Patricia " />
+            </div>
+          </div>
+          <div className="about__me__content">
+            <div className="about__cards">
+              <article className="about__card">
+                <FaAward className="about__icon" />
+                <h5>Experience</h5>
+                <small>1+ Years</small>
+              </article>
+              <article className="about__card">
+                <FiUsers className="about__icon" />
+                <h5>Clients</h5>
+                <small>3 world widely</small>
+              </article>
+              <article className="about__card">
+                <VscFolderLibrary className="about__icon" />
+                <h5>Projects</h5>
+                <small>2 Completed</small>
+              </article>
+            </div>
+            <p>
+              I am a student currently studying software engineering at the Faculty of
+              Engineering and Technology(FET) of the University of Buea.I hold a
+              French-speaking baccalaureate c sexion at bilingual grammar school 
+              Buea and decided to continue my studies in English at the university of Buea.
+               I can therefore express myself in both French and
+              English (average).
+            </p>
+            <a href="#contact" className="btn btn-primary">
+              Let's talk
+            </a>
+          </div>
+        </div>
+      </section>
+    );
+}
+
+export default About
